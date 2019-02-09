e2177503b1b20feb504d9938a22ebf37c9e2ef62
Williaan Souza <williaanlopes@gmail.com>
2019-02-09 21:12:49 -0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# registro-de-ponto
+# Registro de Ponto
+========
+
+Projeto simples em JAVA usando os frameworks: Primefaces Mobile e Hibernate
+
+Utilizado na disciplina de Dispositivos Moveis.
+
+## Mais informações
+--------
+
+Mais informações sobre o projeto na pasta **DOCUMENTS**
+
+Lá estão disponíveis a documentação, diagrama de classes e dump do banco de dados.
+
--- src/main/resources/META-INF/sql/dados-iniciais.sql
+++ src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -13,9 +13,6 @@ insert into disciplina_horario (id_disciplina, turno, dia, de_horario, ate_horar
 insert into disciplina_horario (id_disciplina, turno, dia, de_horario, ate_horario) values (2, 'Noite', 'Quarta-feira', '20:30', '22:10');
 insert into disciplina_horario (id_disciplina, turno, dia, de_horario, ate_horario) values (2, 'Manha', 'Terca-feira', '07:30', '08:30');
 insert into disciplina_horario (id_disciplina, turno, dia, de_horario, ate_horario) values (4, 'Noite', 'Quinta-feira', '18:50', '22:10');
-insert into disciplina_horario (id_disciplina, turno, dia, de_horario, ate_horario) values (3, 'Noite', 'Sexta-feira', '18:50', '22:10');
-insert into disciplina_horario (id_disciplina, turno, dia, de_horario, ate_horario) values (3, 'Noite', 'Quarta-feira', '18:50', '22:10');
-insert into disciplina_horario (id_disciplina, turno, dia, de_horario, ate_horario) values (3, 'Manha', 'Terca-feira', '08:30', '10:50');
 
 insert into professor (nome, sobre_nome, matricula) values ('Cynara', 'Carvalho', '123456');
 insert into professor (nome, sobre_nome, matricula) values ('Carlos', 'Alberto', '456789');
